0f870d88ac582623448e2e98b46f7291a196f2e1
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-10-11 15:16:07 -0500

    added apiroutes.js

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+const path = require('path')
+const fs = require('fs')
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../db/db.json'))
+})
+
+router.post('/notes', (req, res) => {
+    let db = fs.readFileSync('db/db.json')
+    db = JSON.parse(db)
+    res.join(db)
+
+    let userNote = {
+        title: req.body.title,
+        text: req.body.text,
+    }
+
+    db.push(userNote)
+    fs.writeFileSync('db/db.json', JSON.stringify(db))
+    res.json(db)
+})
+
+module.exports = router
--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,18 @@
-const app = express()
+const express = require('express')
 const fs = require('fs')
 const apiRoutes = require('./routes/apiRoutes')
-const htmlRoutes = require('/routes/htmlRoutes')
+// const htmlRoutes = require('/routes/htmlRoutes')
 const PORT = process.env.PORT || 3001
+
+const app = express()
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static(__dirname))
 
 app.use('/api', apiRoutes)
-app.use('/', htmlRoutes)
+// app.use('/', htmlRoutes)
 
 app.listen(PORT, () => {
-    console.log('server is now on port ${PORT}')
+    console.log(`server is now on port ${PORT}`)
 })
