e688007a7e8d9eb1601850575f687c8dafcfaf25
Kevin Quinlan <108549782+Quinlan246@users.noreply.github.com>
2022-10-12 18:13:57 -0500

    added delete route and finised readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Note-Taker
+
+This application is used to add and delete notes as needed 
+
+[view deployed heroku app here](https://dry-beyond-74063.herokuapp.com/)
+
+## Install 
+
+You will need to first install the needed packages with the command line using npm i 
+
+## Usage
+
+Once all packages are installed you can open the server using node server.js in the terminal.
+
+This application is deployed on heroku to be tested.
+
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const path = require('path')
 const fs = require('fs')
+var uniqid = require('uniqid')
 
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'))
@@ -14,11 +15,23 @@ router.post('/notes', (req, res) => {
     let userNote = {
         title: req.body.title,
         text: req.body.text,
+        id: uniqid(),
     }
 
     db.push(userNote)
     fs.writeFileSync('db/db.json', JSON.stringify(db))
     res.json(db)
+})
+
+router.delete('/notes/:id', (req, res) => {
+
+    let db = JSON.parse(fs.readFileSync('db/db.json'))
+
+    let deleteNotes = db.filter(item => item.id !== req.params.id);
+
+    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+    res.json(deleteNotes);
+
 })
 
 module.exports = router
